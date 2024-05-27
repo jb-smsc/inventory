a9642be12c100d9878305bffa580bc8b8c9c6552
mitmat <michaelmatiu@gmail.com>
2024-05-27 11:10:56 +0200

    suggestions for DEM GMBA merge

--- R/Overview/Height_Area_DEM.R
+++ R/Overview/Height_Area_DEM.R
@@ -44,7 +44,7 @@ rm(raster_crs)
 # Calculation ####
 
 # Define the breaks and labels for altitude bins
-breaks <- seq(0, 6000, by = 500)
+breaks <- seq(-500, 9000, by = 500)
 labels <- paste0("(", breaks[-length(breaks)], "-", breaks[-1], "] m")
 
 
@@ -58,13 +58,18 @@ altitude <- extract(DEM, inventory_GMBA)
 # Add the altitude information to the inventory_GMBA DataFrame
 inventory_GMBA$altitude <- altitude
 
+# check differences
+# plot(inventory_GMBA$`Altitude (m)`, inventory_GMBA$altitude)
+# plot(inventory_GMBA$`Altitude (m)` - inventory_GMBA$altitude)
+# hist(inventory_GMBA$`Altitude (m)` - inventory_GMBA$altitude, 50)
+
 # Categorize the altitude information into altitude bins
-inventory_GMBA$altitude_bin <- cut(inventory_GMBA$altitude, breaks = breaks, labels = labels, include.lowest = TRUE)
+# inventory_GMBA$altitude_bin <- cut(inventory_GMBA$altitude, breaks = breaks, labels = labels, include.lowest = TRUE)
 
 # Calculate the area of each altitude bin in each region of "Level_01" to "Level_04"
-area_by_altitude_and_region <- inventory_GMBA %>%
-  group_by(Level_01, Level_02, Level_03, Level_04, altitude_bin) %>%
-  summarise(area = sum(!is.na(altitude)) * (1.85)^2)  # Use 1.85 km as the resolution
+# area_by_altitude_and_region <- inventory_GMBA %>%
+#   group_by(Level_01, Level_02, Level_03, Level_04, altitude_bin) %>%
+#   summarise(area = sum(!is.na(altitude)) * (1.85)^2)  # Use 1.85 km as the resolution
 
 # 3. Count the number of stations in each altitude bin and region
 stations_by_altitude_and_region <- inventory_GMBA %>%
@@ -73,38 +78,38 @@ stations_by_altitude_and_region <- inventory_GMBA %>%
 
 # Remove the geometries from the DataFrames
 stations_by_altitude_and_region_nogeom <- st_set_geometry(stations_by_altitude_and_region, NULL)
-area_by_altitude_and_region_nogeom <- st_set_geometry(area_by_altitude_and_region, NULL)
+# area_by_altitude_and_region_nogeom <- st_set_geometry(area_by_altitude_and_region, NULL)
 
 # Perform the join and calculate the number of stations per km²
-stations_by_altitude_and_region <- stations_by_altitude_and_region_nogeom %>%
-  left_join(area_by_altitude_and_region_nogeom, by = c("Level_01", "Level_02", "Level_03", "Level_04", "altitude_bin")) %>%
-  mutate(stations_per_km2 = n_stations / area)
+# stations_by_altitude_and_region <- stations_by_altitude_and_region_nogeom %>%
+#   left_join(area_by_altitude_and_region_nogeom, by = c("Level_01", "Level_02", "Level_03", "Level_04", "altitude_bin")) %>%
+#   mutate(stations_per_km2 = n_stations / area)
 
 
 
-# For Level_01
-stations_by_altitude_and_region_Level_01 <- inventory_GMBA %>%
-  group_by(Level_01, altitude_bin) %>%
-  summarise(n_stations = n(), area = sum(!is.na(altitude)) * (1.85)^2) %>%
-  mutate(stations_per_km2 = n_stations / area)
-
-# For Level_02
-stations_by_altitude_and_region_Level_02 <- inventory_GMBA %>%
-  group_by(Level_02, altitude_bin) %>%
-  summarise(n_stations = n(), area = sum(!is.na(altitude)) * (1.85)^2) %>%
-  mutate(stations_per_km2 = n_stations / area)
-
-# For Level_03
-stations_by_altitude_and_region_Level_03 <- inventory_GMBA %>%
-  group_by(Level_03, altitude_bin) %>%
-  summarise(n_stations = n(), area = sum(!is.na(altitude)) * (1.85)^2) %>%
-  mutate(stations_per_km2 = n_stations / area)
-
-# For Level_04
-stations_by_altitude_and_region_Level_04 <- inventory_GMBA %>%
-  group_by(Level_04, altitude_bin) %>%
-  summarise(n_stations = n(), area = sum(!is.na(altitude)) * (1.85)^2) %>%
-  mutate(stations_per_km2 = n_stations / area)
+# # For Level_01
+# stations_by_altitude_and_region_Level_01 <- inventory_GMBA %>%
+#   group_by(Level_01, altitude_bin) %>%
+#   summarise(n_stations = n(), area = sum(!is.na(altitude)) * (1.85)^2) %>%
+#   mutate(stations_per_km2 = n_stations / area)
+# 
+# # For Level_02
+# stations_by_altitude_and_region_Level_02 <- inventory_GMBA %>%
+#   group_by(Level_02, altitude_bin) %>%
+#   summarise(n_stations = n(), area = sum(!is.na(altitude)) * (1.85)^2) %>%
+#   mutate(stations_per_km2 = n_stations / area)
+# 
+# # For Level_03
+# stations_by_altitude_and_region_Level_03 <- inventory_GMBA %>%
+#   group_by(Level_03, altitude_bin) %>%
+#   summarise(n_stations = n(), area = sum(!is.na(altitude)) * (1.85)^2) %>%
+#   mutate(stations_per_km2 = n_stations / area)
+# 
+# # For Level_04
+# stations_by_altitude_and_region_Level_04 <- inventory_GMBA %>%
+#   group_by(Level_04, altitude_bin) %>%
+#   summarise(n_stations = n(), area = sum(!is.na(altitude)) * (1.85)^2) %>%
+#   mutate(stations_per_km2 = n_stations / area)
 
 
 
@@ -148,32 +153,41 @@ for(i in 1:nrow(GMBA_1)) {
 areas <- list()
 
 # Schleife über jede Zeile in GMBA_1
-for(i in 1:nrow(GMBA_1)) {
+# for(i in 1:10) { # test a few rows
+for(i in 1:nrow(GMBA)) {
   # Extrahieren Sie das Polygon für die aktuelle Zeile
-  polygon <- st_geometry(GMBA_1[i, ])
+  polygon <- st_geometry(GMBA[i, ])
   
   # Konvertieren Sie das sf-Objekt in ein Spatial-Objekt
   polygon_sp <- as(polygon, "Spatial")
   
   # Zuschneiden des Rasters auf das Polygon
   cropped_raster <- crop(DEM, polygon_sp)
+  cropped_raster_masked <- mask(cropped_raster, polygon_sp)
   
   # Extrahieren Sie die Höhenwerte aus dem zugeschnittenen Raster
-  altitudes <- values(cropped_raster)
+  altitudes <- values(cropped_raster_masked)
   
   # Teilen Sie die Höhenwerte in die definierten Bereiche ein
   altitude_bins <- cut(altitudes, breaks = breaks, labels = labels, include.lowest = TRUE)
   
   # Berechnen Sie die Fläche für jedes Höhenintervall
-  area_per_bin <- tapply(altitudes, altitude_bins, function(x) sum(!is.na(x)) * res(cropped_raster)[1] * res(cropped_raster)[2])
+  # area_per_bin <- tapply(altitudes, altitude_bins, function(x) sum(!is.na(x)) * res(cropped_raster)[1] * res(cropped_raster)[2])
+  npixel_per_bin <- tapply(altitudes, altitude_bins, function(x) sum(!is.na(x)))
   
   # Speichern Sie die Flächen in der Liste
-  areas[[i]] <- area_per_bin
+  # areas[[i]] <- npixel_per_bin
+  
+  # better: save as tbl
+  tbl_out <- as_tibble(npixel_per_bin, rownames = "altitude_bin") %>% 
+    cbind(GMBA_V2_ID = GMBA$GMBA_V2_ID[i])
+  
+  areas[[i]] <- tbl_out
 }
 
 # Jetzt enthält 'areas' die Flächen der Polygone in 'GMBA_1' für jedes Höhenintervall
 
-
+df_areas <- bind_rows(areas)
 
 
 # Erstellen Sie eine leere Liste, um die Anzahl der Stationen pro Höhenstufe zu speichern
